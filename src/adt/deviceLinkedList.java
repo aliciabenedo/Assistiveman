41e114144d2799db82cc8a7dfeb75cd3e2342031
aliciabenedo <116078757+aliciabenedo@users.noreply.github.com>
2026-03-15 02:44:02 +0000

    Add files via upload

---
+++ src/adt/deviceLinkedList.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package adt;
+import model.assistiveDevice;
+
+/**
+ *
+ * @author NagbontianAliciaBene
+ */
+public class deviceLinkedList {
+    rivate Node head;
+    c
+    
+}
