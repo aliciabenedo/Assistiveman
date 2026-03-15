5c951a5b0fdeaa7b42879b6f2a20948ed744a00f
aliciabenedo <116078757+aliciabenedo@users.noreply.github.com>
2026-03-15 03:13:57 +0000

    Update deviceLinkedList.java

--- src/adt/deviceLinkedList.java
+++ src/adt/deviceLinkedList.java
@@ -10,7 +10,33 @@ import model.assistiveDevice;
  * @author NagbontianAliciaBene
  */
 public class deviceLinkedList {
-    rivate Node head;
-    c
+    private Node head;
+    class Node{
+        assistiveDevice device;
+        Node next;
+        
+        Node (assistiveDevice device){
+            this.device=device;
+            this.next=null;
+            
+            
+        }
+    }
+    
+    public void addDevice(assistiveDevice device){
+        Node newNode=new Node(device);
+        
+        if (head==null){
+            head=newNode;
+        }else{
+            Node temp=head;
+            while(temp.next !=null){
+                temp=temp.next;
+            }
+            temp.next=newNode;
+        }
+        
+        
+    }
     
 }
