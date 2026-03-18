3c0c19d17aecf7b518824cf56a88bb9733717459
aliciabenedo <116078757+aliciabenedo@users.noreply.github.com>
2026-03-18 14:15:38 +0000

    Update deviceStack.java

--- src/adt/deviceStack.java
+++ src/adt/deviceStack.java
@@ -11,24 +11,24 @@ import model.assistiveDevice;
  * @author NagbontianAliciaBene
  */
 public class deviceStack {
-    private Stack<assistiveDevice>stack;
+    private Stack<assistiveDevice>stack;//the stack to store devices
     
     public deviceStack(){
-        stack=new Stack<>();
+        stack=new Stack<>(); //initialisies stack
     }
-    //pushing
+    //pushing device onto stack
     public void pushDevice(assistiveDevice device){
-        stack.push(device);
+        stack.push(device);//adds device to the top
         System.out.println("device has been pushd"+device.getDeviceName());
     }
     
-    //this is popping
+    //this is popping device from stack
     public assistiveDevice popDevice(){
         if(stack.isEmpty()){
             System.out.println("stack is emptys");
             return null;
         }
-        return stack.pop();
+        return stack.pop();//reoves then returns it to the top of the edevice
     }
     
     //displaying
@@ -38,7 +38,7 @@ public class deviceStack {
             return;
         }
         System.out.println("stack devices");
-        for(assistiveDevice d : stack){
+        for(assistiveDevice d : stack){ //this loops through the stack
         System.out.println(
         d.getDeviceID()+"-"+
         d.getDeviceName());
