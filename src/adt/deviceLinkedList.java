efa8bf60e2fe638860424255b9f278cfe70c7c37
aliciabenedo <116078757+aliciabenedo@users.noreply.github.com>
2026-03-18 14:16:59 +0000

    Update deviceLinkedList.java

--- src/adt/deviceLinkedList.java
+++ src/adt/deviceLinkedList.java
@@ -10,30 +10,31 @@ import model.assistiveDevice;
  * @author NagbontianAliciaBene
  */
 public class deviceLinkedList implements deviceOperation{
-    private Node head;
+    private Node head; //first node in my list
 
     @Override
     public void dislayDevices() {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
     }
     
-    class Node {
+    class Node { //inner class node to store device
         assistiveDevice device;
                 Node next;
                 
-                public Node(assistiveDevice device){
+                public Node(assistiveDevice device){ //my constructer
                     this.device=device;
                     this.next=null;
                 }
     }
 
-    public void displayDevices() { 
-        Node temp = head;
-        if (temp == null){
+    public void displayDevices() { //methodto displayy all the devices
+        Node temp = head; //starting from the head
+        if (temp == null){ //checks if my list is empty
             System.out.println("no devices have been found");
-            return;
+            return; //stops method
         }
-        while (temp != null){
+        while (temp != null){ //looping through the list
+            
             System.out.println(
                     
             temp.device.getDeviceID()+ "-"+
@@ -45,64 +46,65 @@ public class deviceLinkedList implements deviceOperation{
         }
     }
 
-    public assistiveDevice searchDevice(String id) {
+    public assistiveDevice searchDevice(String id) { //method that searchs device by id
         Node temp=head;
-        while(temp != null){
-            if(temp.device.getDeviceID().equals(id)){
-                return temp.device;
+        while(temp != null){ //loops
+            if(temp.device.getDeviceID().equals(id)){//checks if ids matches
+                return temp.device; //returns the found device
             }
-            temp=temp.next;
+            temp=temp.next; //moves to next  node
             }
-return null;
+return null; //returns null if its nto found
 
         }
         
 
 
     public void removeDevice(String id) {
-        if (head==null)return;
-        if(head.device.getDeviceID().equals(id)){
-            head=head.next;
+        if (head==null)return; //if the lift is empty it stops
+        if(head.device.getDeviceID().equals(id)){//checks if first node is the oe to delete
+            head=head.next;//moves the head to nextnode
             return;
         }
         
         Node temp=head;
         
-        while (temp.next != null){
+        while (temp.next != null){//loops through list
+            //checks the next node
             if(temp.next.device.getDeviceID().equals(id)){
-                temp.next=temp.next.next;
+                temp.next=temp.next.next; //this will skip node
                 return;
             }
-            temp=temp.next;
+            temp=temp.next;//moves on
         }
         
     }
     
     public void addDevice(assistiveDevice device){
-        Node newNode=new Node(device);
+        Node newNode=new Node(device);//thsi creates a new node
         
-        if (head==null){
-            head=newNode;
+        if (head==null){ //if its empty
+            head=newNode;//sets it at the first node
         }else{
             Node temp=head;
-            while(temp.next !=null){
+            while(temp.next !=null){//goes to the lat node
                 temp=temp.next;
             }
-            temp.next=newNode;
+            temp.next=newNode;//add new node at the end
         }
         
         
     }
     
-    public int countDevices(){
+    public int countDevices(){ //recurtive mth to count dev
         return countRecursive(head);
     }
-    private int countRecursive(Node node){
-        if (node==null){
+    private int countRecursive(Node node){ //a helper recursive met
+        if (node==null){ //this is the base
             return 0;
         }
         
-        return 1+ countRecursive(node.next);
+        return 1+ countRecursive(node.next);//this is the recursive call
     }
     
 }
